9be0d385a68185967769a09f030972367f15535e
Ridhan-Github <100027036+Ridhan-Github@users.noreply.github.com>
2022-02-23 12:26:22 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -58,20 +58,35 @@ ABOUT_ME = """
 
 
     BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @No_OnE_Kn0wS_Me For more Details.. " 
+
     DOWNLOAD_START = "<b>Downloading To My server !! Pls Wait</b>"
+
     UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TeleGram</b>"
+
     AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our Channel @MaI_BoTs ❤️</b>"
+
     SAVED_CUSTOM_THUMB_NAIL = "**Yᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴀᴠᴇᴅ ✓**"
+
     DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
+
     SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
-    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
+
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Rᴇᴘʟʏ ᴛᴏ ᴛʜᴀᴛ ꜰɪʟᴇ ᴡɪᴛʜ /rename ɴᴇᴡ ꜰɪʟᴇɴᴀᴍᴇ.ᴇxᴛᴇɴsɪᴏɴ</b>"
+
     REPLY_TO_FILE_FOR_CONVERT = "<b> Please Reply To An File With /c2v To Convert It Into Streamable video File</b>"
+
     CUSTOM_CAPTION_UL_FILE = " **ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴜsᴇ ᴍᴇ**
 
 **ᴊᴏɪɴ - @DisneyBots** "
+
     NO_THUMB_FOUND = "**Nᴏ ᴛʜᴜᴍʙɴᴀɪʟ ꜰᴏᴜɴᴅ 😟**"
+
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
+
     RENAME_HELP = """Here are The Available Commands In Reanme \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
+
     C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
+
     THUMBNAIL_HELP = """Here Are The Available Commands In Custom Thumbnail \n\n\n ▪️ Send A Photo To Set The Custom Thumbnail \n▪️ <code>/showthumb</code> : For Checking The Current Thumbnail \n▪️<code>/delthumb</code> : For Deleting The Current Saved Thumbnail"""
+
     CCAPTION_HELP = """Here Are The Available Commands In Custom Caption \n\n\n ▪️<code>/scaption</code> Use This Command To Save Your Custom Caption \n<b>Usage:</b> <code>/scaption your caption text</code> \n\n<b>[You Can Use</b> <code>{filename}</code> <b>For showing new file name in the caption]</b> """
